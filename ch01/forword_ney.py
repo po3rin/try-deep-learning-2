63683f0c5b55d07875e1fb561c0009c1377dc39f
po3rin <abctail30@gmail.com>
2018-11-13 00:38:44 +0900

    add layer

--- ch01/forword_ney.py
+++ ch01/forword_ney.py
@@ -1,15 +1,48 @@
-import numpy as numpy
+import numpy as np
 
 # p15
 class Sigmoid:
     def __init__(self):
         self.params = []
-    def forword(self, x):
+    def forward(self, x):
         return 1 / 1 + np.exp(-x)
 
 class Affine:
     def __init__(self, W, b):
         self.params = [W, b]
-    def forword(self, x):
+    def forward(self, x):
         W, b = self.params
         return np.dot(x, W) + b
+
+class TwoLayerNet:
+    def __init__(self, input_size, hidden_size, output_size):
+        I, H, O = input_size, hidden_size, output_size
+
+        # 重みとバイアスの初期化
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+
+        # レイヤの生成
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        # すべての重みをリストにまとめる
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+
+    def predict(self, x):
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+x = np.random.randn(10, 2)
+print(x)
+model = TwoLayerNet(2, 4, 3)
+s = model.predict(x)
+print(s)
