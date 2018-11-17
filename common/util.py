1d2a944e0d68c79857a5cd6e0dde16745a70b9f8
po3rin <abctail30@gmail.com>
2018-11-18 00:29:31 +0900

    add ch03

--- common/util.py
+++ common/util.py
@@ -21,6 +21,90 @@ def preprocess(text):
 
     return corpus, word_to_id, id_to_word
 
+
+def create_co_matrix(corpus, vocab_size, window_size=1):
+    corpus_size = len(corpus)
+    co_matrix = np.zeros((vocab_size, vocab_size), dtype=np.int32)
+
+    for idx, word_id in enumerate(corpus):
+        for i in range(1, window_size + 1):
+            left_idx = idx - 1
+            right_idx = idx + 1
+
+            if left_idx >= 0:
+                left_word_id = corpus[left_idx]
+                co_matrix[word_id, left_word_id] += 1
+
+            if right_idx < corpus_size:
+                right_word_id = corpus[right_idx]
+                co_matrix[word_id, right_word_id] += 1
+
+    return co_matrix
+
+
+def cos_similarity(x, y, eps=1e-8):
+    nx = x / (np.sqrt(np.sum(x ** 2)) + eps)
+    ny = y / (np.sqrt(np.sum(y ** 2)) + eps)
+    return np.dot(nx, ny)
+
+
+def most_similar(query, word_to_id, id_to_word, word_matrix, top=5):
+    '''類似単語の検索
+    :param query: クエリ（テキスト）
+    :param word_to_id: 単語から単語IDへのディクショナリ
+    :param id_to_word: 単語IDから単語へのディクショナリ
+    :param word_matrix: 単語ベクトルをまとめた行列。各行に対応する単語のベクトルが格納されていることを想定する
+    :param top: 上位何位まで表示するか
+    '''
+    if query not in word_to_id:
+        print('%s is not found' % query)
+        return
+
+    print('\n[query] ' + query)
+    query_id = word_to_id[query]
+    query_vec = word_matrix[query_id]
+
+    vocab_size = len(id_to_word)
+
+    similarity = np.zeros(vocab_size)
+    for i in range(vocab_size):
+        similarity[i] = cos_similarity(word_matrix[i], query_vec)
+
+    count = 0
+    for i in (-1 * similarity).argsort():
+        if id_to_word[i] == query:
+            continue
+        print(' %s: %s' % (id_to_word[i], similarity[i]))
+
+        count += 1
+        if count >= top:
+            return
+
+
+def ppmi(C, verbose=False, eps = 1e-8):
+    '''PPMI（正の相互情報量）の作成
+    :param C: 共起行列
+    :param verbose: 進行状況を出力するかどうか
+    :return:
+    '''
+    M = np.zeros_like(C, dtype=np.float32)
+    N = np.sum(C)
+    S = np.sum(C, axis=0)
+    total = C.shape[0] * C.shape[1]
+    cnt = 0
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            pmi = np.log2(C[i, j] * N / (S[j]*S[i]) + eps)
+            M[i, j] = max(0, pmi)
+
+            if verbose:
+                cnt += 1
+                if cnt % (total//100) == 0:
+                    print('%.1f%% done' % (100*cnt/total))
+    return M
+
+
 def clip_grads(grads, max_norm):
     total_norm = 0
     for grad in grads:
---
+++ play.py
@@ -0,0 +1,4 @@
+
+import numpy as np
+M = np.zeros_like([[4,4,0,0],[22,2,2,2]], dtype=np.float32)
+print(M.shape)
